e2a74091925543440aa4b7ade4d615fddff490da
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-02-13 01:18:47 +0300

    feat: add Carbon Fields settings.

---
+++ wp-content/themes/stroiparitet/carbon-fields/sections.php
@@ -0,0 +1,116 @@
+<?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+Container::make( 'post_meta', __( 'Поля контента', THEME_NAME ) )
+	->where( 'post_type', '=', 'page' )
+	->add_fields( [
+		Field::make( 'complex', 'page_sections', __( 'Секции страницы', THEME_NAME ) )
+			->set_layout( 'tabbed-horizontal' )
+
+			// Hero section.
+			->add_fields( 'hero_section', __( 'Основная', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок (тег <br/> для переноса)', THEME_NAME ) )
+					->set_width( 34 ),
+				Field::make( 'text', 'desc', __( 'Описание', THEME_NAME ) )
+					->set_width( 33 ),
+				Field::make( 'image', 'bg_image', __( 'Фоновое изображение', THEME_NAME ) )
+				     ->set_width( 33 ),
+				Field::make( 'date', 'date', __( 'Дата окончания акции', THEME_NAME ) )
+					->set_storage_format( 'd:m:Y' )
+					->set_width( 50 ),
+				Field::make( 'text', 'button_label', __( 'Текст на кнопке', THEME_NAME ) )
+					->set_width( 50 )
+			] )
+
+			// Info section.
+			->add_fields( 'info_section', __( 'Информация', THEME_NAME ), [
+				Field::make( 'complex', 'info', __( 'Информация', THEME_NAME ) )
+					->set_layout( 'tabbed-horizontal' )
+					->set_max( 3 )
+					->add_fields( [
+						Field::make( 'image', 'image', __( 'Изображение', THEME_NAME ) )
+							->set_width( 34 ),
+						Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) )
+							->set_width( 33 ),
+						Field::make( 'text', 'desc', __( 'Описание', THEME_NAME ) )
+							->set_width( 33 )
+					] )
+			] )
+
+			// About Us section.
+			->add_fields( 'about_section', __( 'О нас', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) )
+					->set_width( 50 ),
+				Field::make( 'textarea', 'text', __( 'Текст', THEME_NAME ) )
+					->set_width( 50 )
+			] )
+
+			// Products section.
+			->add_fields( 'products_section', __( 'Продукция', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) )
+					->set_width( 50 ),
+				Field::make( 'text', 'button_label', __( 'Текст на кнопке', THEME_NAME ) )
+					->set_width( 50 ),
+				Field::make( 'complex', 'products', __( 'Товары', THEME_NAME ) )
+					->set_layout( 'tabbed-horizontal' )
+					->add_fields( [
+						Field::make( 'image', 'image', __( 'Изображение', THEME_NAME ) )
+							->set_width( 34 ),
+						Field::make( 'text', 'title', __( 'Название', THEME_NAME ) )
+							->set_width( 33 ),
+						Field::make( 'text', 'desc', __( 'Описание', THEME_NAME ) )
+							->set_width( 33 )
+					] )
+			] )
+
+			// Icon & text repeater section.
+			->add_fields( 'steps_section', __( 'Шаги', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) ),
+				Field::make( 'complex', 'steps', __( 'Шаги', THEME_NAME ) )
+					->set_layout( 'tabbed-horizontal' )
+					->add_fields( [
+						Field::make( 'image', 'image', __( 'Изображение', THEME_NAME ) )
+							->set_width( 34 ),
+						Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) )
+							->set_width( 33 ),
+						Field::make( 'textarea', 'desc', __( 'Описание', THEME_NAME ) )
+							->set_width( 33 )
+					] )
+			] )
+
+			// Certificates section.
+			->add_fields( 'certificates_section', __( 'Сертификаты', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) ),
+				Field::make( 'complex', 'certificates', __( 'Сертификаты', THEME_NAME ) )
+					->set_layout( 'tabbed-horizontal' )
+					->add_fields( [
+						Field::make( 'image', 'certificate', __( 'Изображение', THEME_NAME ) )
+					] )
+			] )
+
+			// Testimonials section.
+			->add_fields( 'testimonials_section', __( 'Отзывы', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) ),
+				Field::make( 'complex', 'testimonials', __( 'Отзывы', THEME_NAME ) )
+					->set_layout( 'tabbed-horizontal' )
+					->add_fields( [
+						Field::make( 'image', 'image', __( 'Изображение', THEME_NAME ) ),
+						Field::make( 'image', 'avatar', __( 'Фото автора', THEME_NAME ) )
+							->set_width( 25 ),
+						Field::make( 'text', 'name', __( 'Имя', THEME_NAME ) )
+							->set_width( 25 ),
+						Field::make( 'text', 'position', __( 'Должность', THEME_NAME ) )
+							->set_width( 25 ),
+						Field::make( 'text', 'text', __( 'Текст', THEME_NAME ) )
+							->set_width( 25 )
+					] )
+			] )
+
+			// Latest news section.
+			->add_fields( 'news_section', __( 'Новости', THEME_NAME ), [
+				Field::make( 'text', 'title', __( 'Заголовок', THEME_NAME ) )
+			] )
+	] );
+
--- wp-content/themes/stroiparitet/functions.php
+++ wp-content/themes/stroiparitet/functions.php
@@ -7,6 +7,8 @@
  * @subpackage stroiparitet
  */
 
+use Carbon_Fields\Carbon_Fields;
+
 define( 'THEME_VERSION', mt_rand() );
 define( 'THEME_URI', get_template_directory_uri() );
 const THEME_NAME = 'stroiparitet';
@@ -19,6 +21,13 @@ function critick_load_theme_dependencies(): void
 {
 	// Register theme menus.
 	register_nav_menus( ['header_menu' => esc_html__( 'Меню в шапке сайта', THEME_NAME )] );
+
+	// Register Carbon Fields.
+	Carbon_Fields::boot();
+	add_action( 'carbon_fields_register_fields', function(){
+		require_once( 'carbon-fields/theme-settings.php' );
+		require_once( 'carbon-fields/sections.php' );
+	} );
 
 	// Please place all custom functions declarations in this file.
 	require_once( 'theme-functions/theme-functions.php' );
@@ -34,11 +43,12 @@ function critick_init_theme(): void
 	remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 	remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
 
+	// Enable post thumbnails.
+	add_theme_support( 'post-thumbnails' );
 
 	// Custom image sizes.
 	 add_image_size( 'full-hd', 1920 );
 	 add_image_size( 'sp_icon', 0, 100 );
-	 add_image_size( 'sp_icon_sm', 24, 24 );
 	 add_image_size( 'sp_vertical', 300, 400 );
 	 add_image_size( 'sp_avatar', 120, 120 );
 	 add_image_size( 'sp_avatar', 120, 120 );
